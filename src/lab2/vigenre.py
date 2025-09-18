0c00aaef3f4a68865204322b59a51b5a4d4569ce
ngdis13 <the.n_kocherovaaa@bk.ru>
2025-09-18 10:53:13 +0300

    Добавила решение для шифра Виженера и прошла доктесты

--- src/lab2/vigenre.py
+++ src/lab2/vigenre.py
@@ -8,9 +8,35 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
-    ciphertext = ""
-    # PUT YOUR CODE HERE
-    return ciphertext
+    lst_plaintect_code = [ord(i) for i in plaintext]
+    lst_keyword_code = [
+        ord(i.lower()) - ord("a") for i in keyword
+    ]  # Приведение ключа к нижнему регистру
+    lst_result = []
+    indx_code = 0
+    for code in lst_plaintect_code:
+        if chr(code) == chr(code).upper():
+            start_indx = ord("A")
+        else:
+            start_indx = ord("a")
+        if chr(code).isalpha():
+            if indx_code < len(lst_keyword_code):
+                formula = (code - start_indx + lst_keyword_code[indx_code]) % 26 + start_indx
+                indx_code += 1
+            else:
+                formula = (code - start_indx + lst_keyword_code[0]) % 26 + start_indx
+                indx_code = 1
+            lst_result.append(formula)
+        else:
+            if indx_code < len(lst_keyword_code):
+                indx_code += 1
+            else:
+                indx_code = 1
+            lst_result.append(code)  # Если это не буква, оставляем символ без изменений
+    string_result = ""
+    for code in lst_result:
+        string_result += chr(code)
+    return string_result
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
@@ -23,6 +49,29 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
     'ATTACKATDAWN'
     """
-    plaintext = ""
-    # PUT YOUR CODE HERE
-    return plaintext
+    lst_keyword_code = [
+        ord(i.lower()) - ord("a") for i in keyword
+    ]  # Приведение ключа к нижнему регистру
+    lst_result = []
+    indx_code = 0
+    for code in ciphertext:
+        if code.isalpha():  # Проверяем, является ли символ буквой
+            if code.isupper():
+                start_indx = ord("A")  # Начало алфавита для заглавных букв
+            else:
+                start_indx = ord("a")  # Начало алфавита для строчных букв
+            # Считаем формулу расшифровки
+            shift = lst_keyword_code[indx_code % len(lst_keyword_code)]  # Сдвиг по ключевому слову
+            formula = (
+                ord(code) - start_indx - shift + 26
+            ) % 26 + start_indx  # Применяем формулу сдвига
+            lst_result.append(chr(formula))  # Добавляем расшифрованный символ
+            indx_code += 1  # Переход к следующему символу ключа
+        else:
+            lst_result.append(code)  # Если это не буква, оставляем символ без изменений
+            indx_code += 1
+
+    string_result = ""
+    for i in lst_result:
+        string_result += i
+    return string_result
